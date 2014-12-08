fd9c6c24c0443e9644b468a3440c1ee79a5f2397
awashburn awashburn <awashburn1web@hotmail.com>
2014-12-07 23:13:45 -0600

    Corrected errors, documented code, refactored

--- SecretSanta/CyclicArrangement.hs
+++ SecretSanta/CyclicArrangement.hs
@@ -1,25 +1,48 @@
-{-# LANGUAGE TypeSynonymInstances #-}
-module SecretSanta.CyclicArangement () where
+{-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module SecretSanta.CyclicArrangement
+  ( CyclicArrangement
+  ) where
 
 import Control.Applicative ((<$>),(<*>))
 import Control.Arrow (second)
 import Data.List     (delete,find,minimumBy)
-import Data.Map      (Map,assocs)
+import Data.Map      (assocs,fromList)
 import Data.Foldable (foldl')
-import Data.Maybe    (fromJust,isJust,mapMaybe)
+import Data.Maybe    (mapMaybe)
 import Data.Ord      (comparing)
 import Data.Tree
-import SecretSanata.Types
-import SecretSanata.ConstraintMap
+import SecretSanta.Types
 
+
+-- | Represents a valid secret santa arrangement
+-- | which is also a Hamiltonian Cycle
 instance Constrainable CyclicArrangement where
-  feasibleArrangements = feasibleHamiltonianCycles
+  feasibleArrangements = feasibleCyclicArrangements
+  toArrangement        = cycleToMap
 
-mapToTree :: Eq a => Map a [a] -> Tree a
+
+-- | Converts a CyclicArrangement to a normal Arrangement
+cycleToMap :: CyclicArrangement -> Arrangement
+cycleToMap     [] = fromList []
+cycleToMap (x:xs) = fromList $ cycleToMap' (x:xs)
+  where
+    cycleToMap'       [] = []
+    cycleToMap'      [y] = [(y,x)]
+    cycleToMap' (y:z:zs) =  (y,z) : cycleToMap' (z:zs)
+
+-- | List of all hamiltonian cycles satisfying the constraints  
+feasibleCyclicArrangements :: ConstraintMap -> [CyclicArrangement]
+feasibleCyclicArrangements xs = cycles len $ mapToTree xs
+  where len = foldl' (\x _ -> x+1) 0 xs
+
+-- | Builds an intermidate Tree data structure used for cycle search
+mapToTree :: ConstraintMap -> Tree Name
 mapToTree = constructNode
         <$> assocs
-        <*> (minimumBy (comparing (length .snd)) . assocs)
+        <*> (minimumBy (comparing (length . snd)) . assocs)
 
+-- | Recursively builds Tree from associating list
 constructNode :: Eq a => [(a,[a])] -> (a,[a]) -> Tree a
 constructNode ys (x,xs)
   -- if the "MapList" is empty, we cannot recurse
@@ -36,16 +59,14 @@ constructNode ys (x,xs)
     -- ignore nodes with
     xs' = mapMaybe (flip find ys' . (\y -> (==y).fst)) xs
 
-cycles :: Int -> Tree a -> [[a]]
+-- | Augmented Depth-Limited Search on tree structure
+-- | to find hamiltonian cycles satisfying the constraints
+cycles :: Int -> Tree Name -> [CyclicArrangement]
 cycles 1 tree = [[rootLabel tree]]
 cycles n tree
   | (null . subForest) tree = []
   | otherwise = concatMap
-              ( fmap ((rootLabel tree) :)
+              ( fmap (rootLabel tree :)
               . filter (not.null)
-              . cycles2 (n-1)
+              . cycles (n-1)
               ) $ subForest tree
-
-feasibleCyclicArrangements :: Eq a => ConstraintMap -> [CyclicArranagement]
-feasibleCyclicArrangements xs = cycles len $ mapToTree xs
-  where len = foldl' (\x y -> x+1) 0 xs
